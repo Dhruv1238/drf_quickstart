5f539d0fea1bf9ff6bc764ceaf64ac25136b3bbb
Dhruv1238 <dhruv4075@gmail.com>
2023-06-24 02:30:46 +0530

    MUI configured

--- frontend/react_django_quickstart/src/pages/Data.jsx
+++ frontend/react_django_quickstart/src/pages/Data.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect , useState} from 'react'
 import { useParams } from 'react-router-dom'
 import {useNavigate} from 'react-router'
+import { Button } from "@mui/material";
+
 
 
 
@@ -57,19 +59,23 @@ const Data = ({ match }) => {
     <>
         <div className=' p-2 text-center'>
           <h1>Single Data With id= {id}</h1>
-        <p className='p-2 text-center'>Type: {data?.type}</p>
-        <p className='p-2 text-center'>Contetn: {data?.value}</p>
-        <p className='p-2 text-center'>Created Time: {data?.created_time}</p>
-        <p className='p-2 text-center'>Updated Time: {data?.updated_time}</p>
+        <p className='p-2'>Type: {data?.type}</p>
+        <p className='p-2'>Contetn: {data?.value}</p>
+        <p className='p-2'>Created Time: {data?.created_time}</p>
+        <p className='p-2'>Updated Time: {data?.updated_time}</p>
 
               <br />
               
-              
+
         </div>
         <input className='p-1 rounded-full mx-auto flex bg-slate-200'  type="text" name="vlaue" placeholder="Value" />
         <button className='bg-yellow-500 text-white m-2 p-2 rounded-xl w-fit hover:bg-yellow-600 transition-all duration-300 ease-in-out flex mx-auto' onClick={handleUpdate} type='submit'>Update</button>
         <button className='bg-yellow-500 text-white m-2 p-2 rounded-xl w-fit hover:bg-yellow-600 transition-all duration-300 ease-in-out flex mx-auto' type='submit' onClick={handleDelete}>Delete</button>
-        
+        <div className='flex justify-center'>
+        <Button variant="contained" color="primary">
+           Hello MUI
+        </Button>
+        </div>
 
     </>
   )
